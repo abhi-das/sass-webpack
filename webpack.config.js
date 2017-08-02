254dae5b78e679ac582234feda30b0c6c3ad37c0
Adii <abhishekdass08@gmail.com>
2017-08-01 22:21:26 -0400

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+	entry: [
+		'./src/main.js',
+		'./src/scss/main.scss'
+	],
+	output: {
+		filename: 'dist/bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				loader: ExtractTextPlugin.extract({
+          			loader: 'css-loader'
+				}),
+		        options: {
+		          includePath: "./css/plain_css.css"
+		        }
+			},
+			{ // sass / scss loader for webpack
+		        test: /\.(sass|scss)$/,
+		        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+	      	}
+
+		]
+	},
+	plugins: [
+		new ExtractTextPlugin({
+			filename: 'dist/[name].bundle.css',
+			allChunks: true
+		})
+	]
+};
